729c0aebbc9a654bc6bfceb87e1e440a750b248b
Sebastian Gibb <mail@sebastiangibb.de>
2017-07-01 16:37:54 +0200

    import xml_text

--- R/package.R
+++ R/package.R
@@ -50,5 +50,5 @@
 #' @import methods
 #' @import Biobase
 #' @importFrom utils packageVersion
-#' @importFrom xml2 read_xml xml_find_all xml_attrs
+#' @importFrom xml2 read_xml xml_find_all xml_attrs xml_text
 NULL
