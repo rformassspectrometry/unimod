e9b00b4d1f6f4135159aed837a863b1bff674eed
Sebastian Gibb <mail@sebastiangibb.de>
2017-07-01 13:53:14 +0200

    replace unimod_tables.xml with unimod.xml

--- inst/scripts/getunimod.sh
+++ inst/scripts/getunimod.sh
@@ -1,13 +1,13 @@
-wget http://www.unimod.org/xml/unimod_tables.xml
+wget http://www.unimod.org/xml/unimod.xml
 
-new='./unimod_tables.xml'
-org='../extdata/unimod_tables.xml'
+new='./unimod.xml'
+org='../extdata/unimod.xml'
 
 if cmp -s "$new" "$org"
 then
     echo "Unimod has not changed."
     rm $new
-else 
+else
     echo "New unimod data. Replacing old"
     mv -b $new $org
 fi
