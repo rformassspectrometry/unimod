9609c71b359ed48b7d19a07867a1a7867d6163f1
Sebastian Gibb <mail@sebastiangibb.de>
2018-01-03 12:13:07 +0100

    Add helper functions to turn composition into character and vice versa

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,35 @@
+#' character to composition
+#'
+#' @param x character
+#' @return named numeric
+#' @noRd
+.character2composition <- function(x) {
+    if (!is.character(x)) {
+        stop("'x' must be a 'character'")
+    }
+    s <- strsplit(x, "[[:space:]]")[[1L]]
+    nms <- gsub("[^[:alpha:]]+", "", s)
+    v <- as.integer(gsub("[^[:digit:]]+", "", s))
+    v[is.na(v)] <- 1L
+    names(v) <- nms
+    v
+}
+
+#' Composition to character
+#'
+#' @param x numeric, named
+#' @return character
+#' @noRd
+.composition2character <- function(x) {
+    if (!is.numeric(x)) {
+        stop("'x' must be a 'numeric'")
+    }
+    if (is.null(names(x))) {
+        stop("'x' must be a named 'numeric'")
+    }
+    paste0(names(x), "(", x, ")", collapse=" ")
+}
+
 #' Test for Modification class
 #' @param object object to test
 #' @return TRUE if object is a Modification otherwise fails with an error
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -5,6 +5,21 @@ test_that(".isModification", {
   expect_error(unimod:::.isModification(1L), "has to be an 'Modification' object")
 })
 
+test_that(".composition2character", {
+  expect_error(unimod:::.composition2character("foo"), "must be a 'numeric'")
+  expect_error(unimod:::.composition2character(1:3), "must be a named")
+  expect_equal(unimod:::.composition2character(c(H=1, P=1, O=4)),
+               "H(1) P(1) O(4)")
+})
+
+test_that(".character2composition", {
+  expect_error(unimod:::.character2composition(1:3), "must be a 'character'")
+  expect_equal(unimod:::.character2composition("H(1) P(1) O(4)"),
+               c(H=1, P=1, O=4))
+  expect_equal(unimod:::.character2composition("H P O(4)"),
+               c(H=1, P=1, O=4))
+})
+
 test_that(".unimodDb", {
   expect_equal(unimod:::.unimodDb(),
                read_xml(system.file(file.path("extdata", "unimod.xml"),
