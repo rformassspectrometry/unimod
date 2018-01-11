d2b6d737abb100363452b6726837f2cd5c5716b3
Sebastian Gibb <mail@sebastiangibb.de>
2018-01-11 21:08:11 +0100

    Remove old unit tests

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,10 +1,5 @@
 context("utils")
 
-test_that(".isModification", {
-  expect_true(unimod:::.isModification(new("Modification")))
-  expect_error(unimod:::.isModification(1L), "has to be an 'Modification' object")
-})
-
 test_that(".composition2character", {
   expect_error(unimod:::.composition2character("foo"), "must be a 'numeric'")
   expect_error(unimod:::.composition2character(1:3), "must be a named")
@@ -18,10 +13,4 @@ test_that(".character2composition", {
                c(H=1, P=1, O=4))
   expect_equal(unimod:::.character2composition("H P O(4)"),
                c(H=1, P=1, O=4))
-})
-
-test_that(".unimodDb", {
-  expect_equal(unimod:::.unimodDb(),
-               read_xml(system.file(file.path("extdata", "unimod.xml"),
-                                    package="unimod")))
 })
