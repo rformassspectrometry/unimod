aca40e5204549b3515eac4c4a5d7618e0267499f
Sebastian Gibb <mail@sebastiangibb.de>
2018-01-12 19:54:03 +0100

    Test dataset creation

---
+++ tests/testthat/test_datasets.R
@@ -0,0 +1,20 @@
+context("datasets")
+
+skip_if_not_installed("xml2")
+
+xml <- unimod:::.unimodDb()
+
+test_that("aminoacids", {
+    data(aminoacids, package="unimod")
+    expect_equal(unimod:::.aminoacids(xml), aminoacids)
+})
+
+test_that("elements", {
+    data(elements, package="unimod")
+    expect_equal(unimod:::.elements(xml), elements)
+})
+
+test_that("modifications", {
+    data(modifications, package="unimod")
+    expect_equal(unimod:::.modifications(xml), modifications)
+})
