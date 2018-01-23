1fc20f958e4be8f60a10ee2308739007d66cee4c
Sebastian Gibb <mail@sebastiangibb.de>
2018-01-23 21:05:04 +0100

    Add .unimodMass, .unimodSequence, and .countSite

--- R/mass.R
+++ R/mass.R
@@ -4,12 +4,152 @@
 #' @param type character, mono or average mass
 #' @return list of numeric (mass values)
 #' @noRd
-.aamass <- function(x, type=c("mono", "average")) {
-    if (match.arg(type) == "mono") {
-        m <- aminoacids$MonoMass
-    } else {
-        m <- aminoacids$AvgMass
-    }
+.aamass <- function(x, type=c("MonoMass", "AvgMass")) {
+    m <- aminoacids[, match.arg(type)]
     names(m) <- aminoacids$OneLetter
     lapply(.string2character(x), function(xx)m[xx])
 }
+
+#' Calculate a single modification for a given sequence
+#'
+#' Throws a message if the default rule is applied and not verified (added to
+#' the switch statement) by a human
+#'
+#' @param x character, sequence
+#' @param id character, modification id
+#' @param type character, mono or average mass
+#' @param msg logical, show message if default was used?
+#' @return numeric (mass values)
+#' @noRd
+.unimodMass <- function(x, id, type=c("MonoMass", "AvgMass"), msg=TRUE) {
+    stopifnot(is.character(x) && is.character(id) && length(id) == 1L)
+    stopifnot(id %in% modifications$Id)
+    type <- match.arg(type)
+    switch(id,
+        ## 765: Met-loss
+        "Met-loss:P-M" =
+            # N-terminal initiator methionine is removed by a methionine
+            # aminopeptidase from proteins where the residue following the
+            # methionine is Ala, Cys, Gly, Pro, Ser, Thr or Val. This is
+            # generally the final N-terminal state for proteins where the
+            # following residue was a Cys, Pro or Val.
+            grepl("^M([ACGPSTV])", x) * modifications[id, type],
+        ## 766: Met-loss+Acetyl
+        "Met-loss+Acetyl:P-M" =
+            # The N-terminal initiator methionine is removed by a methionine
+            # aminopeptidase from proteins whose residue following the
+            # methionine is Ala, Cys, Gly, Pro, Ser, Thr or Val. Proteins
+            # whose following residue was Ala, Gly, Ser or Thr are then
+            # acetylated by an N(alpha)-acetyltransferase on the new
+            # N-terminus.
+            grepl("^M([AGST])", x) * modifications[id, type] +
+            grepl("^M([CPV])", x) * modifications["Met-loss:P-M", type],
+        ## default verified:
+        ## 1: Acetyl
+        "Acetyl:K" =, "Acetyl:N-term" =, "Acetyl:C" =, "Acetyl:S" =,
+        "Acetyl:P-N-term" =, "Acetyl:T" =, "Acetyl:Y" =, "Acetyl:H" =,
+        "Acetyl:R" =,
+        ## 4: Carbamidomethyl
+        "Carbamidomethyl:C" =, "Carbamidomethyl:K" =,
+        "Carbamidomethyl:N-term" =, "Carbamidomethyl:H" =,
+        "Carbamidomethyl:D" =, "Carbamidomethyl:E" =, "Carbamidomethyl:S" =,
+        "Carbamidomethyl:T" =, "Carbamidomethyl:Y" =, "Carbamidomethyl:U" =,
+        "Carbamidomethyl:M" =, "Carbamidomethyl:M:NL" =,
+        ## 21: Phospho
+        "Phospho:T" =, "Phospho:S" =, "Phospho:Y" =, "Phospho:D" =,
+        "Phospho:H" =, "Phospho:C" =, "Phospho:R" =, "Phospho:K" =,
+        "Phospho:T:NL" =, "Phospho:S:NL" =
+        {
+            .countSite(x, modifications[id, "Site"]) * modifications[id, type]
+        },
+        {
+            if (msg) {
+                message(
+                    "Applying the default rule for the modification: ", id,
+                    "\nPlease create an issue on: https://github.com/",
+                    "ComputationalProteomicsUnit/unimod/issues/new ",
+                    "\nto let us implement the correct rule or if the default ",
+                    "one is already correct we could remove this message."
+                )
+            }
+            .countSite(x, modifications[id, "Site"]) * modifications[id, type]
+        }
+    )
+}
+
+#' Modify the sequence for a single modification
+#'
+#' @param x character, sequence
+#' @param id character, modification id
+#' @return character (sequence)
+#' @noRd
+.unimodSequence <- function(x, id) {
+    stopifnot(is.character(x) && is.character(id) && length(id) == 1L)
+    stopifnot(id %in% modifications$Id)
+    switch(id,
+        ## 765: Met-loss
+        "Met-loss:P-M" =,
+        ## 766: Met-loss+Acetyl
+        "Met-loss+Acetyl:P-M" =
+            gsub("^M([ACGPSTV])", "\\1", x),
+        x
+    )
+}
+
+#' Find site occurrence
+#'
+#' @param x character, sequence
+#' @param site character
+#' @param fixed logical
+#' @return numeric
+#' @noRd
+.countSite <- function(x, site, fixed=TRUE) {
+    stopifnot(is.character(x) && is.character(site) && length(site) == 1L)
+    if (endsWith(site, "term")) {
+        rep.int(1L, length(x))
+    } else {
+        nchar(x) - nchar(gsub(site, "", x, fixed=TRUE))
+        ## while looking a little bit odd it is much faster than:
+        # vapply(
+        #     gregexpr(pattern=site, text=x, fixed=fixed),
+        #     function(rx)sum(rx > 0L), double(1L),
+        #     USE.NAMES=FALSE
+        # )
+    }
+}
+
+#' Calculate mass for a given aminoacid sequence and apply modifications
+#'
+#' @param x character, sequence
+#' @param type character, mono or average mass
+#' @param fixedModifications character/data.frame
+#' @param variableModifications data.frame
+#' @return list of numeric (mass values)
+#' @noRd
+#.mass <- function(x, type=c("MonoMass", "AvgMass"),
+#                  fixedModifications=NULL,
+#                  variableModifications=NULL) {
+#    if (is.character(fixedModifications)) {
+#        i <- match(fixedModifications, modifications$Id)
+#        if (anyNA(i)) {
+#            stop("Given 'fixedModifications' are not part of the ",
+#                 "unimod::modifications data set!")
+#        }
+#        modifications <- modifications[i,]
+#    }
+#
+#    if (is.data.frame(fixedModifications)) {
+#        if (!all(c("Id", type, "Site") %in% colnames(fixedModifications))) {
+#            stop("The 'fixedModifications' `data.frame` has to have at least ",
+#                 "the columns 'Id', 'MonoMass'/'AvgMass', and 'Site'!")
+#        }
+#        modifications <- fixedModifications
+#    } else if (!is.null(fixedModifications)) {
+#        stop("'fixedModifications' must be a `character`, `data.frame`, or ",
+#             "`NULL`!")
+#    }
+#
+#    type <- match.arg(type)
+#    m <- .aamass(x, type)
+#}
+#
--- tests/testthat/test_mass.R
+++ tests/testthat/test_mass.R
@@ -1,14 +1,73 @@
 context("mass")
 
 test_that(".aamass", {
-    expect_error(.aamass(1:3))
-    expect_error(.aamass("ACE", "FOO"))
-    expect_equal(.aamass("ACE"), .aamass("ACE", "mono"))
-    expect_equal(.aamass("ACE")[[1]],
+    expect_error(unimod:::.aamass(1:3))
+    expect_error(unimod:::.aamass("ACE", "FOO"))
+    expect_equal(unimod:::.aamass("ACE"), unimod:::.aamass("ACE", "MonoMass"))
+    expect_equal(unimod:::.aamass("ACE")[[1]],
                  c(A=71.037114, C=103.009185, E=129.042593))
-    expect_equal(.aamass("ACE", "average")[[1]],
+    expect_equal(unimod:::.aamass("ACE", "AvgMass")[[1]],
                  c(A=71.0779, C=103.1429, E=129.1140))
-    expect_equal(.aamass(c(foo="ACE", bar="CEA")),
+    expect_equal(unimod:::.aamass(c(foo="ACE", bar="CEA")),
                  list(foo=c(A=71.037114, C=103.009185, E=129.042593),
                       bar=c(C=103.009185, E=129.042593, A=71.037114)))
+})
+
+test_that(".unimodMass", {
+    expect_error(unimod:::.unimodMass(1:3))
+    expect_error(unimod:::.unimodMass("ACE"))
+    expect_error(unimod:::.unimodMass("ACE", "FOO"))
+    expect_error(unimod:::.unimodMass("ACE", c("FOO", "BAR")))
+    expect_equal(unimod:::.unimodMass("ACE", "Met-loss:P-M"), 0)
+    expect_equal(unimod:::.unimodMass("MACE", "Met-loss:P-M"),
+                 unimod:::.unimodMass("MACE", "Met-loss:P-M", "MonoMass"))
+    expect_equal(unimod:::.unimodMass("MACE", "Met-loss:P-M", "MonoMass"),
+                 -131.040485)
+    expect_equal(unimod:::.unimodMass("MACE", "Met-loss:P-M", "AvgMass"),
+                 -131.1961)
+    expect_equal(unimod:::.unimodMass("MCCE", "Met-loss:P-M"),
+                 unimod:::.unimodMass("MCCE", "Met-loss+Acetyl:P-M"))
+    expect_equal(unimod:::.unimodMass("MACE", "Met-loss+Acetyl:P-M"),
+                 -89.02992)
+    expect_equal(unimod:::.unimodMass("ACE", "Acetyl:N-term"), 42.010565)
+    expect_equal(unimod:::.unimodMass("ACE", "Acetyl:C"), 42.010565)
+    expect_equal(unimod:::.unimodMass("ACE", "Acetyl:H"), 0)
+    expect_equal(unimod:::.unimodMass(c("ABE", "ACE"), "Acetyl:C"),
+                 c(0, 42.010565))
+    expect_message(unimod:::.unimodMass("ACE", "Unknown:420:N-term"),
+                   "Applying the default rule for .* create an issue")
+    expect_silent(unimod:::.unimodMass("ACE", "Unknown:420:N-term", msg=FALSE))
+})
+
+test_that(".unimodSequence", {
+    expect_error(unimod:::.unimodSequence(1:3))
+    expect_error(unimod:::.unimodSequence("ACE"))
+    expect_error(unimod:::.unimodSequence("ACE", "FOO"))
+    expect_error(unimod:::.unimodSequence("ACE", c("FOO", "BAR")))
+    expect_equal(unimod:::.unimodSequence("ACE", "Met-loss:P-M"), "ACE")
+    expect_equal(unimod:::.unimodSequence("MACE", "Met-loss:P-M"), "ACE")
+    expect_equal(unimod:::.unimodSequence("MACE", "Met-loss+Acetyl:P-M"),
+                 "ACE")
+    expect_equal(unimod:::.unimodSequence(c("MBCE", "MACE"), "Met-loss:P-M"),
+                 c("MBCE", "ACE"))
+})
+
+test_that(".mass", {
+    #expect_error(.mass(1:3))
+    #expect_error(.mass("ACE", "FOO"))
+    #expect_error(.mass("ACE", fixedModifications="FOO"), "are not part")
+    #expect_error(.mass("ACE", fixedModifications=1:3), "must be a `character`")
+})
+
+test_that(".countSite", {
+    expect_error(unimod:::.countSite(1:3, "C"))
+    expect_error(unimod:::.countSite("ACE", 1:3))
+    expect_error(unimod:::.countSite("ACE", c("A", "C")))
+    expect_equal(unimod:::.countSite("ACE", "Nterm"), 1)
+    expect_equal(unimod:::.countSite("ACE", "Cterm"), 1)
+    expect_equal(unimod:::.countSite("ACE", "C"), 1)
+    expect_equal(unimod:::.countSite(c("ACCE", "ACE"), "Nterm"), c(1, 1))
+    expect_equal(unimod:::.countSite(c("ACCE", "ACE"), "Cterm"), c(1, 1))
+    expect_equal(unimod:::.countSite(c("ACCE", "ACE"), "C"), 2:1)
+    expect_equal(unimod:::.countSite(c("ACCE", "ACE"), "H"), c(0, 0))
 })
